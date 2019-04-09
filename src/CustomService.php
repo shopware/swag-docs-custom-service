5990501f958272402256df92d12e6505315bdbcc
Patrick Stahl <p.stahl@shopware.com>
2019-04-09 09:23:33 +0200

    Switch to new structure

---
+++ src/CustomService.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace Swag\CustomService;
+
+use Shopware\Core\Framework\Plugin;
+
+class CustomService extends Plugin
+{
+
+}
--- src/Service/MyService.php
+++ src/Service/MyService.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types = 1);
 
-namespace CustomService\Service;
+namespace Swag\CustomService\Service;
 
 class MyService
 {
